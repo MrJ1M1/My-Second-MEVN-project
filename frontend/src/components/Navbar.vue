618ec71c3f1d645a07a8000ce1321abe9df4bd2a
Jamshidbek Razzaqov <101242488+MrJ1M1@users.noreply.github.com>
2022-11-20 16:15:57 +0500

    Create Navbar.vue

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-success d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+
+        <router-link to="/" class="text-decoration-none text-dark fw-bold mx-5 fs-3">My First Own Vue.js CRUD</router-link>
+
+      <nav class="d-inline-flex mt-2 mt-md-0 py-2 ms-md-auto">
+        <router-link to="/create" class="btn btn-dark mx-5 text-decoration-none">Create</router-link>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'    
+}
+</script>
+
+<style scoped>
+    
+</style>
